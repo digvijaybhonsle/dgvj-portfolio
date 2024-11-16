725583ccab1a2c6e96405c633dc356df3415211b
digvijaybhonsle <digvijaybhonsle007@gmail.com>
2024-11-16 22:42:40 +0530

    Fix paths for GitHub Pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import dgvj from "../assets/dgvj.jpg";
+import dgvj from "../assets/dgvj.jpg"; // Make sure this image path is correct
 
 const About = () => {
   const [showMore, setShowMore] = useState(false);
@@ -17,9 +17,10 @@ const About = () => {
   }, []);
 
   return (
-    <div className="">
+    <div className="flex flex-col">
       {/* Top Section with Image and Navbar */}
       <div className="flex" style={{ height: "500px" }}>
+        {/* Left side with Image and Navbar */}
         <div className="w-1/2 bg-[#1c1c1c] text-white flex flex-col items-center justify-center">
           <Navbar />
           <div className="flex justify-center items-center mt-6">
@@ -29,30 +30,33 @@ const About = () => {
             >
               <img
                 src={dgvj}
-                alt="Digvijay Bhonsle"
+                alt="Digvijay Bhonsle portrait"
                 className="object-cover w-full h-full"
               />
             </div>
           </div>
         </div>
 
-        {/* Introduction Section */}
+        {/* Right side with Introduction */}
         <div className="w-1/2 bg-gray-300 flex flex-col justify-center items-center space-y-4 p-6">
           <div className={`transition-all duration-1000 transform ${animateText ? "opacity-100 translate-x-0" : "opacity-0 translate-x-20"}`}>
             <h2 className="text-lg font-semibold text-gray-700">Hi, I am</h2>
-            <h1 className="text-3xl font-bold text-gray-800 pt-10 pb-4">
-              Digvijay Bhonsle
-            </h1>
-            <h3 className="text-md font-medium text-gray-600 pb-11">
-              Software Developer Engineer Enthusiast / Problem Solver
-            </h3>
+            <h1 className="text-3xl font-bold text-gray-800 pt-10 pb-4">Digvijay Bhonsle</h1>
+            <h3 className="text-md font-medium text-gray-600 pb-11">Software Developer Engineer Enthusiast / Problem Solver</h3>
             <div className="flex space-x-6 mt-2">
               {/* Social Icons */}
-              {[
-                { href: "https://twitter.com/yourprofile", icon: faTwitter },
-                { href: "https://github.com/digvijaybhonsle", icon: faGithub },
-                { href: "https://www.linkedin.com/in/digvijay-bhonsle/", icon: faLinkedin },
-              ].map(({ href, icon }, index) => (
+              {[{
+                href: "https://twitter.com/yourprofile",
+                icon: faTwitter,
+              },
+              {
+                href: "https://github.com/digvijaybhonsle",
+                icon: faGithub,
+              },
+              {
+                href: "https://www.linkedin.com/in/digvijay-bhonsle/",
+                icon: faLinkedin,
+              }].map(({ href, icon }, index) => (
                 <div
                   key={index}
                   className="p-2 rounded-md bg-gray-200 transition-transform transform duration-300 hover:scale-110 hover:bg-gray-400"
